ca5a1dd7dedf739b0bf5ea6d5de4deee4896182e
ulysses <wx2806784370@outlook.com>
2021-10-15 01:09:38 +0800

    new(lab1)

--- LAB1/main.cpp
+++ LAB1/main.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
-	double deltaX;
-	cout << "Please input deltaX " << endl;
-	cin >> deltaX;
-	Cderivative* example = new Cderivative(deltaX);
-	example->display_function();
-	delete example;
+	/*cout << "Please input deltaX " << endl;*/
+	double deltaXlist[] = { 0.1, 0.01, 0.001, 0.0001, 0.00001 };
+	
+	for (auto deltaX: deltaXlist) {
+		Cderivative* example = new Cderivative(deltaX);
+		example->display_function();
+		delete example;
+	}
+	
+	
 }
